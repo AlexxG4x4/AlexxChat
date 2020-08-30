bc2dcead6f6115f8e370ceda921c8a048c89a51b
Aleksandr <alexgurin86@gmail.com>
2020-08-30 15:42:51 +0300

    Initial commit

---
+++ src/main/java/server/ChatApp.java
@@ -0,0 +1,11 @@
+package server;
+
+import server.service.ServerImpl;
+import java.io.IOException;
+
+public class ChatApp {
+
+    public static void main(String[] args) throws IOException {
+        new ServerImpl();
+    }
+}
